b64c9755ac827736c1c12740265eaac50ed55b3f
wallace-chiu <wallacechiu05@gmail.com>
2025-02-17 04:58:09 -0800

    added sunset and a light for boat

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ let fallingDown = 0; let isFallingDown = false;
 let startHeight;
 let boatLeftRightIncrement = 0; let boatFrontBackIncrement = 0;; boatRotationIncrement = 0.1
 let boatColorIncrementer = 0;
-
+let sunsetChange = 0; let sunsetIncrement = 0.001; let sunGoingDown = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -22,11 +22,25 @@ function setup() {
 
 function draw() {
   //setup background
-  background(135, 206, 235);
+  background(135, 206 - sunsetChange, 235 - sunsetChange);
   camera(0, 200, 800);
   orbitControl();
-  ambientLight(150, 150, 150)
-  directionalLight(255, 255, 255, 0, 500, 0);
+  ambientLight(150, 150 - sunsetChange, 150 - sunsetChange)
+  directionalLight(255, 255 - sunsetChange, 255 - sunsetChange, 0, 500, 0);
+
+  //this equation changes the amount of green and blue
+  //changes it slowly at first, then quicker as the scene gets redder
+  //cause sunsets don't last as long as daytime
+  //no night time because I don't like that
+  sunsetChange += sunsetIncrement * (sunGoingDown ? 1 : -1) + (sunsetIncrement * (sunGoingDown ? 1 : -1) * sunsetChange);
+  if(sunsetChange >= 150)
+  {
+    sunGoingDown = false;
+  }
+  if(sunsetChange <= 0)
+  {
+    sunGoingDown = true;
+  }
   rotateX(-45);
   rotateY(45);
 
@@ -75,6 +89,7 @@ function draw() {
       zOffset += 0.0001;
     }
 
+    
     //doing physics to figure out the movement of the water
 
     //finding the average height of a block and the surrounding 8 blocks
@@ -121,6 +136,11 @@ function draw() {
     //if the boat isn't falling down and the boat is within the generated waters, do the normal animation
     if(!isFallingDown && floor(ballX) >=0 && floor(ballY) >= 0 && floor(ballX) < cols && floor(ballY) < rows)
     {
+      //turns on a light for the boat when it gets darker
+      if(sunsetChange >= 70)
+      {
+        pointLight(105 + sunsetChange, 105 + sunsetChange, 105 + sunsetChange, ballX, sizes[floor(ballX)][floor(ballY)] - 10, ballY);
+      }
       translate(ballX * size - (size * rows/2) + ballXOffset, startHeight - cols - size + heightIncrement, ballY * size - (size * cols/2) + ballYOffset);
     }
     //if the boat is falling down, then we perform the falling animation
@@ -130,6 +150,13 @@ function draw() {
       isFallingDown = true;
       //change average height with an increment (it's addition because that's just how this renderer works)
       avgHeight += fallingDown;
+
+      //turns on a light for the boat when it gets darker
+      if(sunsetChange >= 70)
+      {
+        pointLight(105 + sunsetChange, 105 + sunsetChange, 105 + sunsetChange, ballX, avgHeight, ballY);
+      }
+
       //if you look closely, it's the same command as the if statement, except the 2nd parameter is adapted to just fall
       translate(ballX * size - (size * rows/2) + ballXOffset, avgHeight, ballY * size - (size * cols/2) + ballYOffset);
       //increase the gravity as time goes on
@@ -140,7 +167,7 @@ function draw() {
       if(avgHeight >= 1000)
       {
         //reset the boat physics
-        console.log("resetting");
+        //console.log("resetting");
         ballXOffset = 0;
         ballYOffset = 0;
         ballX = cols / 2;
@@ -198,7 +225,7 @@ function calculateFrontBackDrift(arr, col, row)
   }
   let backAvg = (backCount == 0 ? 0 : (backSum / backCount));
   let frontAvg = (frontCount == 0 ? 0 : (frontSum / frontCount));
-  console.log(backAvg + " " + frontAvg);
+
   return backAvg > frontAvg ? -1 : 1;
 }
 
@@ -225,7 +252,7 @@ function calculateLeftRightDrift(arr, col, row)
   }
   let leftAvg = (leftCount == 0 ? 0 : (leftSum / leftCount));
   let rightAvg = (rightCount == 0 ? 0 : (rightSum / rightCount));
-  console.log(leftAvg + " " + rightAvg);
+
   return leftAvg > rightAvg ? -1 : 1;
 }
 
@@ -250,10 +277,8 @@ function findAvgHeight(arr, col, row)
   //if there is no block to measure, then return 0 and avoid a divide by 0 error
   return count == 0 ? 0 : sum / count;
 }
-{
 
 //resizes window if you adjust it
-}
 function windowResized()
 {
   resizeCanvas(windowWidth, windowHeight);
